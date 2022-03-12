593895ac2d333aad8a5198d6d3653a67afd8b48d
Mayrcon <mayrcon_marlon@hotmail.com>
2022-03-12 09:27:14 -0300

    perfil de desenvolvimento

--- src/main/java/br/com/mayrcon/helpdesk/config/DevConfig.java
+++ src/main/java/br/com/mayrcon/helpdesk/config/DevConfig.java
@@ -1,23 +1,30 @@
 package br.com.mayrcon.helpdesk.config;
 
 import br.com.mayrcon.helpdesk.services.DBService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     private final DBService dbService;
 
-    public TestConfig(DBService dbService) {
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    public DevConfig(DBService dbService) {
         this.dbService = dbService;
     }
 
     @Bean
-    public void instaciaDB() {
-        this.dbService.instanciaDB();
+    public boolean instaciaDB() {
+
+        if(value.equals("create")) {
+            this.dbService.instanciaDB();
+        }
+        return false;
     }
 }
