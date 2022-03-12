0b4c25bc2cc234f0fe551a0c5ca768cda7a41ab9
Mayrcon <mayrcon_marlon@hotmail.com>
2022-03-12 10:17:51 -0300

    Proteção de serialização

---
+++ src/main/java/br/com/mayrcon/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,2 @@
+package br.com.mayrcon.helpdesk.resources;public class TecnicoResource {
+}
---
+++ src/main/java/br/com/mayrcon/helpdesk/services/TecnicoService.java
@@ -0,0 +1,7 @@
+package br.com.mayrcon.helpdesk.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TecnicoService {
+}
